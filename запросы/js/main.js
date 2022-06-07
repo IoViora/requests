8e3ac7496809eec2822afad8828b3fac924413b9
Alymbek <n.m.alymbek@gmail.com>
2022-06-07 10:45:13 +0600

    added some comments for json-server

---
+++ запросы/js/main.js
@@ -0,0 +1,12 @@
+//! Types of requests
+/*
+POST - add some data
+PUT - total change data
+PATCH - partically change data
+DELETE - delete data or smth
+GET - get data or smth
+*/
+//!launch commands json-server
+/*
+json-server -w dg.json -p 8000
+*/
